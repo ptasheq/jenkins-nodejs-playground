44a7423bf592bdd7df8285ac6260dadbc025e601
piotr-ptaszynski <p.p.ptaszynski@gmail.com>
2018-10-22 10:30:55 +0200

    Improve build scripts

--- scripts/helpers/runner.js
+++ scripts/helpers/runner.js
@@ -1,5 +1,7 @@
 const sh = require('shelljs');
 const package_helper = require('./package.js');
+
+sh.config.fatal = true;
 const {SECRET_SSH_KEY, DEPLOY_TO, PORT, USERNAME} = process.env;
 
 function runWhenNewHash(command, payload) {
@@ -12,8 +14,11 @@ function run(command, payload) {
 	if (command === 'ssh_exec') {
 		sh.exec(`ssh -i ${SECRET_SSH_KEY} -p ${PORT} ${USERNAME}@${DEPLOY_TO} "${payload}"`)
 	}
+	else if (command === 'exec') {
+		sh.exec(payload);
+	}
 	else {
-		sh[command](payload);
+		sh[command](...payload);
 	}
 }
 
